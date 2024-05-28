9f4a87e82aea5e38f5a29fd26f0b72a5cfea46d4
Mastershort <BeckerTorsten82@gmail.com>
2024-05-28 20:23:25 +0200

    Update energy_optimizer.py

--- custom_components/energy-optimizer/energy_optimizer.py
+++ custom_components/energy-optimizer/energy_optimizer.py
@@ -1,1 +1,18 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+async def async_setup(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Set up the integration."""
+    # Implementiere hier die Einrichtung deiner Integration.
+    # Dies kann das Laden von Plattformen, das Konfigurieren von Geräten usw. umfassen.
+    # Diese Funktion wird automatisch aufgerufen, wenn die Integration eingerichtet wird.
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Set up the integration for a config entry."""
+    # Implementiere hier spezifische Einrichtung für eine einzelne Konfigurationseintrag.
+    # Diese Funktion wird automatisch aufgerufen, wenn eine Konfigurationseintrag eingerichtet wird.
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Unload the integration for a config entry."""
+    # Implementiere hier spezifische Bereinigungsaufgaben beim Entladen einer Konfigurationseintrag.
+    # Diese Funktion wird automatisch aufgerufen, wenn eine Konfigurationseintrag entfernt wird.
